e4fbd5237f4da4979d105c175a9d6d6212ac7bc1
renaldo_louis <renaldo.louis@commsult.id>
2021-11-12 15:14:44 +0700

    fix display overal

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class SignInPage extends StatelessWidget {
+class SigninPage extends StatefulWidget {
+  // const SigninPage({ Key? key }) : super(key: key);
+
+  @override
+  _SigninPageState createState() => _SigninPageState();
+}
+
+class _SigninPageState extends State<SigninPage> {
+  int _currentIndex = 0;
   final List<String> imageList = [
     "https://www.setaswall.com/wp-content/uploads/2018/08/Spiderman-Wallpaper-76-1280x720.jpg",
     "https://images.hdqwalls.com/download/spiderman-peter-parker-standing-on-a-rooftop-ix-1280x720.jpg",
@@ -9,7 +17,16 @@ class SignInPage extends StatelessWidget {
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvUgui-suS8DgaljlONNuhy4vPUsC_UKvxJQ&usqp=CAU",
   ];
 
-  // const SignInPage({ Key? key }) : super(key: key);
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
+
+  List cardList = ["Item1(), Item2(), Item3(), Item4()"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,165 +39,218 @@ class SignInPage extends StatelessWidget {
       //   ),
       // ),
       body: _buildContent(),
+      bottomNavigationBar: BottomNavigationBar(
+        // backgroundColor: Colors.red,
+        // fixedColor: Colors.red,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.blue,
+            ),
+            label: 'Home',
+            backgroundColor: Colors.black,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            // backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            // backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_alarm),
+            label: "asd",
+            //  backgroundColor: Colors.red,
+          ),
+        ],
+      ),
     );
   }
 
   Container _buildContent() {
     return Container(
       color: Color(0xFF252934),
-      padding: EdgeInsets.only(left: 16, right: 16),
+      padding: EdgeInsets.only(left: 16, right: 16, top: 35),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        // mainAxisAlignment: MainAxisAlignment.start,
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 25.0),
-            child: Column(
-              children: <Widget>[
-                Align(
-                  alignment: Alignment(-1, -1),
-                  child: Text(
-                    "Halo, ",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFFC4C4C4),
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment(-1, -1),
-                  child: Text(
-                    "Sutisna!",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFFA273E0),
-                      fontSize: 28,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: 100,
-            height: 132,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Color(0xFF0C55ED),
-                    Color(0xFFEFAFD5),
-                  ]),
-              borderRadius: BorderRadius.all(
-                (Radius.circular(20)),
-              ),
-            ),
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                      margin:
-                          EdgeInsets.only(top: 20.0, left: 15.0, right: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Align(
-                            child: Text(
-                              "Total Investasi",
-                              style: TextStyle(
-                                  color: Colors.white70,
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 14.0),
-                            ),
-                          ),
-                          Align(
-                            child: Text(
-                              "Transaksi >",
-                              style: TextStyle(
-                                  color: Colors.white70,
-                                  // fontWeight: FontWeight.bold,
-                                  fontSize: 12.0),
-                            ),
-                          ),
-                        ],
-                      )),
-                  Container(
-                    margin: EdgeInsets.only(top: 3.0, left: 15.0, right: 10.0),
-                    child: Align(
-                      alignment: Alignment(-1, 1),
-                      child: Text(
-                        "RP 42.375.000",
-                        style: TextStyle(
-                            color: Colors.white70,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 25.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "Halo,",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFC4C4C4),
+                        fontSize: 14,
                       ),
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10.0, left: 15.0, right: 15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Align(
-                          child: Text(
-                            "Total Keuntungan",
-                            style: TextStyle(
-                                color: Colors.white70,
-                                // fontWeight: FontWeight.bold,
-                                fontSize: 14.0),
-                          ),
+                    Align(
+                      child: Text(
+                        "Sutisna!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFFA273E0),
+                          fontSize: 28,
                         ),
-                        Align(
-                          child: Text(
-                            "Persentase Keuntungan",
-                            style: TextStyle(
-                                color: Colors.white70,
-                                // fontWeight: FontWeight.bold,
-                                fontSize: 14.0),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 25.0),
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(100),
                   ),
-                  Container(
-                    margin: EdgeInsets.only(top: 3.0, left: 15.0, right: 10.0),
-                    child: Row(
-                      children: <Widget>[
-                        Align(
+                ),
+              ),
+            ],
+          ),
+          Row(children: [
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 20),
+                width: 100,
+                height: 132,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Color(0xFF0C55ED),
+                        Color(0xFFEFAFD5),
+                      ]),
+                  borderRadius: BorderRadius.all(
+                    (Radius.circular(20)),
+                  ),
+                ),
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                          margin: EdgeInsets.only(
+                              top: 20.0, left: 15.0, right: 10.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Align(
+                                child: Text(
+                                  "Total Investasi",
+                                  style: TextStyle(
+                                      color: Colors.white70,
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: 14.0),
+                                ),
+                              ),
+                              Align(
+                                child: Text(
+                                  "Transaksi >",
+                                  style: TextStyle(
+                                      color: Colors.white70,
+                                      // fontWeight: FontWeight.bold,
+                                      fontSize: 12.0),
+                                ),
+                              ),
+                            ],
+                          )),
+                      Container(
+                        margin:
+                            EdgeInsets.only(top: 3.0, left: 15.0, right: 10.0),
+                        child: Align(
+                          alignment: Alignment(-1, 1),
                           child: Text(
-                            "RP 3.375.000",
+                            "RP 42.375.000",
                             style: TextStyle(
                                 color: Colors.white70,
-                                fontWeight: FontWeight.w800,
-                                fontSize: 14.0),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24.0),
                           ),
                         ),
-                        SizedBox(width: 68.5),
-                        Align(
-                          child: Text(
-                            "5,2%",
-                            style: TextStyle(
-                                color: Colors.white70,
-                                fontWeight: FontWeight.w800,
-                                fontSize: 14.0),
-                          ),
+                      ),
+                      Container(
+                        margin:
+                            EdgeInsets.only(top: 10.0, left: 15.0, right: 15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Align(
+                              child: Text(
+                                "Total Keuntungan",
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    // fontWeight: FontWeight.bold,
+                                    fontSize: 14.0),
+                              ),
+                            ),
+                            Align(
+                              child: Text(
+                                "Persentase Keuntungan",
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    // fontWeight: FontWeight.bold,
+                                    fontSize: 14.0),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                      Container(
+                        margin:
+                            EdgeInsets.only(top: 3.0, left: 15.0, right: 10.0),
+                        child: Row(
+                          children: <Widget>[
+                            Align(
+                              child: Text(
+                                "RP 3.375.000",
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 14.0),
+                              ),
+                            ),
+                            SizedBox(width: 68.5),
+                            Align(
+                              child: Text(
+                                "5,2%",
+                                style: TextStyle(
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 14.0),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
-          Text(
-            "Menabung Untuk",
-            style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 14.0),
+          ]),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Menabung Untuk",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14.0),
+            ),
           ),
           Container(
             // color: Colors.red,
@@ -189,6 +259,8 @@ class SignInPage extends StatelessWidget {
                 enlargeCenterPage: true,
                 enableInfiniteScroll: true,
                 autoPlay: true,
+                viewportFraction: 0.8,
+                // scrollPhysics: ClampingScrollPhysics(),
               ),
               items: imageList
                   .map((e) => ClipRRect(
@@ -198,7 +270,6 @@ class SignInPage extends StatelessWidget {
                           children: <Widget>[
                             Image.network(
                               e,
-                              width: 1050,
                               height: 350,
                               fit: BoxFit.cover,
                             )
@@ -207,8 +278,38 @@ class SignInPage extends StatelessWidget {
                       ))
                   .toList(),
             ),
+            // child: CarouselSlider(
+            //   options: CarouselOptions(
+            //     height: 200.0,
+            //     autoPlay: true,
+            //     autoPlayInterval: Duration(seconds: 3),
+            //     autoPlayAnimationDuration: Duration(milliseconds: 800),
+            //     autoPlayCurve: Curves.fastOutSlowIn,
+            //     pauseAutoPlayOnTouch: true,
+            //     aspectRatio: 2.0,
+            //     onPageChanged: (index, reason) {
+            //       setState(() {
+            //         _currentIndex = index;
+            //       });
+            //     },
+            //   ),
+            //   items: cardList.map((card) {
+            //     return Builder(builder: (BuildContext context) {
+            //       return Container(
+            //         height: MediaQuery.of(context).size.height * 0.30,
+            //         width: MediaQuery.of(context).size.width,
+            //         child: Card(
+            //           color: Colors.blueAccent,
+            //           child: card,
+            //         ),
+            //       );
+            //     });
+            //   }).toList(),
+            // ),
           ),
           Container(
+            width: 300,
+            height: 150,
             color: Colors.purple,
             child: SizedBox(
               height: 100,
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: SignInPage(),
+      home: SigninPage(),
     );
   }
 }
